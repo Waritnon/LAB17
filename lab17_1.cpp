4cdc0f28e396bebd11da5226856268d8982b0c74
Nonlnwza007 <waritmanokaew@gmail.com>
2022-01-18 22:34:11 +0700

    finished

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,42 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double* a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = (double)(rand()%101)/100;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+	double sum1 = 0;
+	int n = 0;
+	for(int i =0;i < N*M;i++){
+		sum1 = sum1 + *(a+i);
+		if((i+1)%M == 0){
+	        *(b+n) = sum1;
+	        sum1 = 0;
+	        n++;
+		}
+	}
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+	double sum2 = 0;
+	for(int i =0;i < M;i++){
+        for(int I=0;I < N;I++){
+            sum2 = sum2 + *(a+(i+M*I));
+        }
+        *(b+i) = sum2;
+        sum2 = 0;
+	}
+}
